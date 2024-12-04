09fb82253db7e6a17b07fbc7819f5f39a940f2f6
talmaikel <tal.mikel@post.runi.ac.il>
2024-12-04 18:00:01 +0200

    trial 2

--- MyString.java
+++ MyString.java
@@ -54,7 +54,6 @@ public class MyString {
                 return true; 
             }
         }
-    
         return false; 
     }
 }
--- Primes.java
+++ Primes.java
@@ -2,6 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         boolean[] isPrime = new boolean[n+1];
+        double count = 0;
         for (int i = 2; i < n; i++)  isPrime[i] = true;
 
         for (int i = 2; i < Math.sqrt(n+1); i++) {
@@ -12,6 +13,14 @@ public class Primes {
                 i++;
             }
         }
-        for (int i = 2; i < n; i++)  if (isPrime[i] == true) System.out.println(i);
+        
+        System.out.println("Prime numbers up to 100:");
+        for (int i = 2; i < n; i++)  {
+            if (isPrime[i] == true) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("There are "+ (int)count +" primes between 2 and "+n+" ("+ (int)((count/n)*100) +"% are primes)");
 }
 }
